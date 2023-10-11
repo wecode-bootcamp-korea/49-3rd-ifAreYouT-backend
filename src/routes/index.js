e1dd1a7eec2342420c5bde99a56999d9c34133af
hyunjin <alfma123@naver.com>
2023-10-11 20:43:57 +0900

    style: code format 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("OK");
+router.get('/', (req, res, next) => {
+  res.send('OK');
 });
 
 module.exports = router;
