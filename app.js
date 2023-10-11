9c6703ead6ba10dd76547f1d3a08e84c624b1671
hyunjin <alfma123@naver.com>
2023-10-11 20:20:02 +0900

    feat: app.js 최초 세팅

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+app.set("port", process.env.PORT || 8000);
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, _, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, _, res, next) => {
+  res.status(err.status || 500);
+  return res.json({
+    error: `${err.status ? err.status : ""} ${err.message}`,
+  });
+});
+
+module.exports = app;
