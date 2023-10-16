5bb771e5c0c842131668009325e46f07af5614e2
hyunjin <alfma123@naver.com>
2023-10-16 18:41:07 +0900

    chore: questions orders db 스키마 수정

--- db/migrations/20231012080349_create_orders_table.sql
+++ db/migrations/20231012080349_create_orders_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `orders` (
   `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `user_id` integer NOT NULL,
-  `order_no` varchar(255) NOT NULL,
+  `order_no` varchar(20) NOT NULL,
   `order_status` enum('pending', 'purchased', 'canceled') NOT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
