5acf29c9e9033e0eec83619f7fec5b20265bb64c
hyunjin <alfma123@naver.com>
2023-10-18 11:16:28 +0900

    feat: 트랜잭션 함수 분리

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 const { default: Axios } = require('axios');
 const jwt = require('jsonwebtoken');
 
-exports.axios = Axios.create({
+const axios = Axios.create({
   headers: {
     'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
   },
@@ -12,7 +12,7 @@ exports.axios = Axios.create({
  * @param {string} data - 토큰을 만들 키 - 값
  * @returns token
  */
-exports.generateToken = (data) => {
+const generateToken = (data) => {
   return jwt.sign({ data }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
@@ -23,7 +23,7 @@ exports.generateToken = (data) => {
  * @param {*} token - jwt 토큰
  * @returns decode 된 토큰
  */
-exports.verifyToken = (token) => {
+const verifyToken = (token) => {
   return jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
 };
 
@@ -33,7 +33,7 @@ exports.verifyToken = (token) => {
  * @param {string} validationTarget - 검증할 문자열
  * @returns
  */
-exports.isValidData = (reg, validationTarget) => {
+const isValidData = (reg, validationTarget) => {
   return reg.test(validationTarget);
 };
 
@@ -43,7 +43,7 @@ exports.isValidData = (reg, validationTarget) => {
  * @param {string} message - 커스텀 하고 싶은 메시지
  * @returns new Error
  */
-exports.throwError = (code, message) => {
+const throwError = (code, message) => {
   if (!code) return;
   const error = new Error();
   let errorMessage = new Map([
@@ -57,4 +57,34 @@ exports.throwError = (code, message) => {
   error.message = errorMessage.get(code);
   error.status = code;
   throw error;
+};
+
+const useTransaction = async (dataSource, queries) => {
+  const queryRunner = await dataSource.createQueryRunner();
+  await queryRunner.connect();
+  await queryRunner.startTransaction();
+  try {
+    const results = [];
+    for (const query of queries) {
+      const result = await query(queryRunner);
+      results.push(result);
+    }
+    await queryRunner.commitTransaction();
+    return results;
+  } catch (err) {
+    console.error(err);
+    throwError(500, 'transaction failed'),
+      await queryRunner.rollbackTransaction();
+  } finally {
+    await queryRunner.release();
+  }
+};
+
+module.exports = {
+  axios,
+  generateToken,
+  verifyToken,
+  isValidData,
+  throwError,
+  useTransaction,
 };
