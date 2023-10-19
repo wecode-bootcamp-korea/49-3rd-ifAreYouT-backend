0e46135737be9c95d600669842af6e3f8f99c143
hyunjin <alfma123@naver.com>
2023-10-20 00:08:00 +0900

    refactor: orders 라우터에 토큰 검증  미들웨어 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const orderRouter = require('./orderRouter');
 const dummyRouter = require('./dummyRouter');
+const { verificateToken } = require('../middlewares');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('OK');
 });
 router.use('/dummy', dummyRouter);
-router.use('/orders', orderRouter);
+router.use('/orders', verificateToken, orderRouter);
 
 module.exports = router;
