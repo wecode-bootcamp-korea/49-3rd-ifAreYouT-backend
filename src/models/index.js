f098225ac845cb3beaf50894856a0afd7423297a
hyunjin <alfma123@naver.com>
2023-10-20 10:53:29 +0900

    refactor: 내보내기 구조 개선

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,9 @@
-const { getSeatsDataDao, isEventExistDao } = require('./orderDao');
+const orderDao = require('./orderDao');
 const dummyDao = require('./dummyDao');
 const ticketDao = require('./ticketDao');
 
 module.exports = {
-  orderDao: {
-    getSeatsDataDao,
-    isEventExistDao,
-  },
+  orderDao,
   dummyDao,
   ticketDao,
 };
