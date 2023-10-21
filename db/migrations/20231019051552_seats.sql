9ef0bca3666e1ed2b79616cdcfaac3a4edaab23c
hyunjin <alfma123@naver.com>
2023-10-21 11:31:32 +0900

    chore: 중복 수정부분 삭제

--- db/migrations/20231019051552_seats.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-ALTER TABLE seats CHANGE COLUMN `row` row_name VARCHAR(10) NOT NULL;
-ALTER TABLE seats CHANGE COLUMN col col_name integer NOT NULL; 
-
--- migrate:down
-ALTER TABLE seats CHANGE COLUMN row_name `row` VARCHAR(10) NOT NULL;
-ALTER TABLE seats CHANGE COLUMN col_name col integer NOT NULL;
