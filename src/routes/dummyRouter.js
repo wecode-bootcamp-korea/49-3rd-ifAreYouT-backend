f88ec89afcaa1b01a199dd96e3f27c4cb2da6c5e
hyunjin <alfma123@naver.com>
2023-10-21 11:28:29 +0900

    chore: dummy생성 라우터 수정

--- src/routes/dummyRouter.js
+++ src/routes/dummyRouter.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const { dataSource } = require('../models/dataSource');
+const { makeSeatDummy, makeEventSeatDummy } = require('../utils/makedummy');
+const { addSeatsQuery, addEventSeatsQuery } = require('../models').dummyDao;
 const router = express.Router();
 
 router.get('/add_seats', async (_, res) => {
+  await dataSource.query(addSeatsQuery(makeSeatDummy(20, 30, 1, 4)));
   res.json({
     message: 'seat created',
   });
 });
 router.get('/add_event_seats', async (_, res) => {
+  await dataSource.query(addEventSeatsQuery(makeEventSeatDummy(600)));
   res.json({
     message: 'seat created',
   });
