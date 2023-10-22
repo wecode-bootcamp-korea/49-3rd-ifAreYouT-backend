6f363743b0468664a61daa04319b7e8efc6d024a
namsuhan0322 <rnatpakfl@gmail.com>
2023-10-23 02:25:12 +0900

    [fix] router 이름 변경

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -3,7 +3,7 @@ const { orderController } = require('../controllers');
 const { verificateToken } = require('../middlewares')
 const router = express.Router();
 
-router.get("/", verificateToken, orderController.getUserOrder)
+router.get("/details", verificateToken, orderController.getUserOrder)
 
 
 module.exports = router;
