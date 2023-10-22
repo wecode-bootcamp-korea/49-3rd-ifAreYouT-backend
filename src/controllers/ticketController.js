eb7a4fcd48cdfff8047c879cc27c9883ce9a792f
namsuhan0322 <rnatpakfl@gmail.com>
2023-10-23 01:15:44 +0900

    [fix] 포스트맨 성공

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -2,24 +2,24 @@ const { ticketService } = require('../services');
 const { throwError } = require('../utils');
 const { v4: uuidv4 } = require('uuid');
 
-const getTicketInfoByUserId = async (req, res) => {
-  const userId = req.userData
+const getTicketInfoByUserId = async (req, res, next) => {
+  const { userId } = req.query;
   try {
     const ticketInfo = await ticketService.getTicketInfo(userId);
 
-    if (!ticketInfo) {
-      throwError(400, 'NOT_FOUND_ORDER');
+    if (!ticketInfo || ticketInfo.length === 0) {
+      throwError(400, 'NOT_FOUND_TICKETS');
     }
 
     res.status(200).json({
       message: 'GET_TICKETS',
-      data: ticketInfo,
+      data : ticketInfo,
     });
   } catch (error) {
     console.log(error);
-    return res.status(400).json(error);
+    next(error);
   }
-};
+};  
 
 module.exports = {
   getTicketInfoByUserId
