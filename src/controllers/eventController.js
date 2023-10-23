9ff06ffd4db20d7170cc7858b5c23f3e51ab7251
namsuhan0322 <rnatpakfl@gmail.com>
2023-10-23 15:39:09 +0900

    [fix] test, postman 수정 및 성공

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,25 +1,20 @@
 const { eventService } = require('../services');
 const { throwError } = require('../utils');
-const { v4: uuidv4 } = require('uuid');
 
 const getLikedEventsByUser = async (req, res, next) => {
   const { userId } = req.query;
-  const reactionType = req.query['reaction-type'] || 'exited' || 'unexited';
-  try { 
-    if (reactionType === 'unexited') {
-      throwError(400, 'UNEXITED_EVENTS')
-    }
-
+  const reactionType = req.query['reaction-type'] || 'exited';
+  try {
     const likesInfo = await eventService.getLikedEvents(userId, reactionType);
 
     if (!likesInfo || likesInfo.length === 0) {
       throwError(400, 'LIKED_EVENTS_NOT_FOUND');
-    } else {
+    };
+
     res.status(200).json({
       message: 'LIKED_EVENTS_FOUND',
       data: likesInfo,
     });
-    }
   } catch (error) {
     console.log(error);
     next(error);
--- tests/userLike.test.js
+++ tests/userLike.test.js
@@ -1,12 +1,9 @@
 const request = require('supertest');
 const { createApp } = require('../app');
 const { dataSource } = require('../src/models/dataSource');
-const jwt = require('jsonwebtoken');
 
 describe('get events with reaction by user', () => {
   let app;
-  let userId;
-  let accessToken;
 
   beforeAll(async () => {
     app = createApp();
@@ -16,18 +13,6 @@ describe('get events with reaction by user', () => {
       VALUES ('wecode13@gmil.com', 'testUser', '010-1234-5678', 'kakao', '1');
     `);
     await dataSource.query(`
-      INSERT INTO orders (user_id, order_no, order_status)
-      VALUES (1, 123, 'pending');
-    `);
-    await dataSource.query(`
-      INSERT INTO stages (name)
-      VALUES ('testname');
-    `);
-    await dataSource.query(`
-      INSERT INTO seat_grades (grade, price, stage_id)
-      VALUES (1, 1, 1);
-    `);
-    await dataSource.query(`
       INSERT INTO categories (category_name)
       VALUES ('testcategoryname');
     `);
@@ -35,71 +20,69 @@ describe('get events with reaction by user', () => {
       INSERT INTO performers (name)
       VALUES ('testname');
     `);
+    await dataSource.query(`INSERT INTO promotions (id) VALUES (1)`)
     await dataSource.query(`
-      INSERT INTO promotions (id)
-      VALUES (1);
-    `);
-    await dataSource.query('DELETE FROM events');
-    await dataSource.query(`
-      INSERT INTO events (id, title, playtime, description, status, start_date, end_date, sales_start_date, sales_end_date,
+      INSERT INTO events (title, playtime, description, status, start_date, end_date, sales_start_date, sales_end_date,
       stage_id, category_id, performer_id, promotion_id)
-      VALUES (1, 'testtitle', '2hour', 'test', 'merchantable', NOW(), NOW(), NOW(), NOW(), 1, 1, 1, 1);
+      VALUES ('testtitle', '2hour', 'test', 'merchantable', NOW(), NOW(), NOW(), NOW(), 1, 1, 1, 1);
     `);
     await dataSource.query(`
       INSERT INTO event_reactions (id, reaction_type, event_id, user_id)
       VALUES (1, 'exited', 1, 1);
     `);
     await dataSource.query(`
-      INSERT INTO times (id, event_time, event_day, event_id)
-      VALUES (1, '03', '23-03-23', 1);
-    `);
-    await dataSource.query(`
-      INSERT INTO seats (row_name, col_name, stage_id, grade_id)
-      VALUES ('test', 123, 1, 1);
-    `);
-    await dataSource.query(`
-      INSERT INTO event_orders (id, time_id, seat_id, order_id, order_name, ticket_code)
-      VALUES (2, 1, 1, 1, 'testordername', 'testticketcode');
-    `);
-
-    const userData = await dataSource.query('SELECT id FROM users WHERE id = ?', [1]);
-
-    userId = userData.id
-    accessToken = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    INSERT INTO event_images 
+      (id, event_id, detail_image_url, thumbnail_image_url) 
+    VALUES 
+      (1, 1, 'http://example.com/detail_image.jpg', 'http://example.com/thumbnail_image.jpg');
+    `)
   });
 
   afterAll(async () => {
     await dataSource.query(`SET foreign_key_checks = 0;`);
     await dataSource.query(`TRUNCATE users`);
-    await dataSource.query(`TRUNCATE stages`);
-    await dataSource.query(`TRUNCATE seat_grades`);
     await dataSource.query(`TRUNCATE categories`);
     await dataSource.query(`TRUNCATE performers`);
     await dataSource.query(`TRUNCATE events`);
-    await dataSource.query(`TRUNCATE times`);
-    await dataSource.query(`TRUNCATE seats`);
-    await dataSource.query(`TRUNCATE orders`);
     await dataSource.query(`TRUNCATE event_reactions`);
-    await dataSource.query(`TRUNCATE event_orders`);
+    await dataSource.query(`TRUNCATE event_images`);
     await dataSource.query(`TRUNCATE promotions`);
     await dataSource.query(`SET foreign_key_checks = 1;`);
     await dataSource.destroy();
   });
 
-  test('GET_likes: user likes', async () => {
-    const res = await request(app)
-      .get(`/events`)
-      .set('Authorization', `Bearer ${accessToken}`);
-      expect(res.status).toBe(200);
-      expect(res.body.message).toEqual('LIKED_EVENTS_FOUND');
-  });
-
   test('INVALID_LIKES: invalid user likes', async () => {
-    const res = await request(app)
-      .get(`/events`)
-      .set('Authorization', `Bearer ${accessToken}`)
+    await request(app)
+      .get(`/events/likes`)
+      .set('Authorization',
+      `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJJZCI6MX0sImlhdCI6MTY5NzcxOTY0MiwiZXhwIjoxNzAwMzExNjQyfQ.zuVcbarIWTuPPBm7DvoaYRsKGFV8YJPK68fa2gztFeU`
+      )
       .expect('Content-Type', /json/)
       .expect(400)
-      expect(res.body.message).toEqual('LIKED_EVENTS_NOT_FOUND');
+       expect({ error: 'LIKED_EVENTS_NOT_FOUND' });
+  });
+
+  test('GET_likes: user likes', async () => {
+    const res = await request(app)
+    .get(`/events/likes?userId=1&reactionType=exited`)
+    .set('Authorization', 
+    `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJJZCI6MX0sImlhdCI6MTY5NzcxOTY0MiwiZXhwIjoxNzAwMzExNjQyfQ.zuVcbarIWTuPPBm7DvoaYRsKGFV8YJPK68fa2gztFeU`
+    )
+
+    expect(res.body).toEqual(
+      {
+        "data": [
+            {
+                "event_id": 1,
+                "eventName": "아리아나 그란데 내한",
+                "eventDate": "2023-07-31T15:00:00.000Z",
+                "performer": "ariana grande",
+                "category": "콘서트",
+                "image_url": "http://example.com/thumbnail_image.jpg"
+            }
+        ],
+        "message": "LIKED_EVENTS_FOUND"
+    }
+    )
   });
 });
