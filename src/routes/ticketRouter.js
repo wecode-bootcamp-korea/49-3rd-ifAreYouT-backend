634aa6926ea5e459e17b0839c572b3d191f3acd1
namsuhan0322 <rnatpakfl@gmail.com>
2023-10-23 20:13:18 +0900

    [fix] 라우터 토큰 위치 변경

--- src/routes/ticketRouter.js
+++ src/routes/ticketRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { ticketController } = require('../controllers');
-const { verificateToken } = require('../middlewares')
 const router = express.Router();
 
 router.get("/", ticketController.getTicketInfoByUserId)
