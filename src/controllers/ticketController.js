f42a35d47543d9e960d8e2972360ff701c5afe42
namsuhan0322 <rnatpakfl@gmail.com>
2023-10-25 17:55:40 +0900

    [fix] 함수 수정

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -1,24 +1,24 @@
 const { ticketService } = require('../services');
 const { throwError } = require('../utils');
 
-const getTicketInfoByUserId = async (req, res, next) => {
+const getTicketInfoByUserId = async (req, res) => {
   const { userId } = req.userData;
   try {
     const ticketInfo = await ticketService.getTicketInfo(userId);
 
-    if (!ticketInfo || ticketInfo.length === 0) {
-      throwError(400, 'NOT_FOUND_TICKETS');
+    if (!ticketInfo) {
+      throwError(400, 'NOT_FOUND_ORDER');
     }
 
-    res.status(200).json({
+    return res.status(200).json({
       message: 'GET_TICKETS',
-      data : ticketInfo,
+      data: ticketInfo,
     });
   } catch (error) {
     console.log(error);
-    next(error);
+    return res.status(400).json(error);
   }
-};  
+};
 
 module.exports = {
   getTicketInfoByUserId
