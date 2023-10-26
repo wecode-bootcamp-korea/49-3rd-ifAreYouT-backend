a022c3410cfa7127d60a277115d81e58119d19d4
namsuhan0322 <rnatpakfl@gmail.com>
2023-10-26 15:59:36 +0900

    [fix] 컨플릭트 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,13 @@ const dummyRouter = require('./dummyRouter');
 const { verificateToken } = require('../middlewares');
 const ticketRouter = require('./ticketRouter');
 const router = express.Router();
+const { eventRouter } = require('./eventRoute');
 
 router.get('/', (req, res) => {
   res.send('OK');
 });
+
+router.use('/events', eventRouter);
 router.use('/dummy', dummyRouter);
 router.use('/orders', verificateToken, orderRouter);
 router.use('/tickets', verificateToken, ticketRouter);
