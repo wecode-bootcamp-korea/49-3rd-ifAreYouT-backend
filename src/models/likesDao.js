b3669975d8ce865f99a61a6fbacce31d4add6ef9
namsuhan0322 <rnatpakfl@gmail.com>
2023-10-26 20:18:50 +0900

    [fix] 빈 배열 수정

--- src/models/likesDao.js
+++ src/models/likesDao.js
@@ -1,7 +1,7 @@
 const { dataSource } = require('./dataSource');
 
 const getLikedEventsByUserId = async (userId, reactionType) => {
-    const user = await dataSource.query(
+    const likesInfo = await dataSource.query(
       `
     SELECT
       e.id AS event_id,
@@ -13,12 +13,12 @@ const getLikedEventsByUserId = async (userId, reactionType) => {
     JOIN event_reactions AS er ON e.id = er.event_id
     JOIN performers AS p ON e.performer_id = p.id
     JOIN categories AS c ON e.category_id = c.id
-    JOIN event_images AS ei ON e.id = ei.event_id
     WHERE er.user_id = ? AND er.reaction_type = ?;
     `,
       [userId, reactionType],
     );
-    return user;
+    console.log(likesInfo);
+    return likesInfo;
   };
   
   module.exports = {
